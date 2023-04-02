c5f5c930e5167cfa8fe5b2072ea7d4f25715e5b2
chisun <csjoung@gmail.com>
2023-04-02 10:35:39 +0900

    styled

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import { gql, useQuery } from "@apollo/client";
 import { Link } from "react-router-dom";
 
@@ -6,29 +7,91 @@ const ALL_MOVIES = gql`
     allMovies {
       title
       id
-    }
-    allTweets {
-      id
-      text
-      author {
-        fullName
-      }
+      poster_path
     }
   }
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
+const MoviesGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+  place-items: center;
+`;
+
+const PosterContainer = styled.div`
+  height: 800px;
+  border-radius: 7px;
+  width: 100%;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  background-color: transparent;
+`;
+
+const PosterBg = styled.div`
+  background-image: url(${(props) => props.background});
+  height: 100%;
+  width: 100%;
+  background-size: cover;
+  background-position: center center;
+  border-radius: 7px;
 `;
 
 export default function Movies() {
   const { loading, error, data } = useQuery(ALL_MOVIES);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
+  console.log(data);
   return (
-    <ul>
-      <h1>Movies</h1>
-      {data.allMovies.map((movie) => (
-        <li key={movie.id}>
-          <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
-        </li>
-      ))}
-    </ul>
+    <Container>
+      <Header>
+        <Title>Apollo Movies</Title>
+      </Header>
+      {loading && <Loading>Loading...</Loading>}
+      <MoviesGrid>
+        {data?.allMovies?.map((movie) => (
+          <PosterContainer key={movie.id}>
+            <Link to={`/movies/${movie.id}`}>
+              <PosterBg
+                background={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+              />
+            </Link>
+          </PosterContainer>
+        ))}
+      </MoviesGrid>
+    </Container>
   );
 }
